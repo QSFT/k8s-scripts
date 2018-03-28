a05e12a388dbadbdbdcbcc8e8b70e4866bb3af5a
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2018-03-28 17:20:41 +0300

    disable add host to knownhosts warning

--- nodes_disk.sh
+++ nodes_disk.sh
@@ -2,9 +2,13 @@ echo "checking disk usage on nodes"
 
 NODES=`kubectl get nodes --no-headers | awk '{print $1}'`  
 
+function get_disk_usage {
+  ssh -q -oServerAliveInterval=5 -oConnectTimeout=10 -oConnectionAttempts=1 -oStrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null azureuser@$1 df -$2 | grep sda
+}
+
 for n in $NODES;do
    echo "node: $n"
-   ssh -oServerAliveInterval=5 -oConnectTimeout=10 -oConnectionAttempts=1 -oStrictHostKeyChecking=no azureuser@$n df -h | grep sda
-   ssh -oServerAliveInterval=5 -oConnectTimeout=10 -oConnectionAttempts=1 -oStrictHostKeyChecking=no azureuser@$n df -i | grep sda
+   get_disk_usage $n "h"
+   get_disk_usage $n "i"
 done
 
