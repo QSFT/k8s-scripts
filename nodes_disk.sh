1fe626e21e1c3bd03b41db41738395f21f7ef95a
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2018-03-28 15:37:52 +0300

    add azureuser for ssh to node

--- nodes_disk.sh
+++ nodes_disk.sh
@@ -4,7 +4,7 @@ NODES=`kubectl get nodes --no-headers | awk '{print $1}'`
 
 for n in $NODES;do
    echo "node: $n"
-   ssh -oServerAliveInterval=5 -oConnectTimeout=10 -oConnectionAttempts=1 -oStrictHostKeyChecking=no $n df -h | grep sda
-   ssh -oServerAliveInterval=5 -oConnectTimeout=10 -oConnectionAttempts=1 -oStrictHostKeyChecking=no $n df -i | grep sda
+   ssh -oServerAliveInterval=5 -oConnectTimeout=10 -oConnectionAttempts=1 -oStrictHostKeyChecking=no azureuser@$n df -h | grep sda
+   ssh -oServerAliveInterval=5 -oConnectTimeout=10 -oConnectionAttempts=1 -oStrictHostKeyChecking=no azureuser@$n df -i | grep sda
 done
 
