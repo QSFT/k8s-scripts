51b12f57f7cf5e9c888d6a3d2db0e385a7bae3ba
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2018-03-29 11:27:06 +0300

    revert

--- nodes_disk.sh
+++ nodes_disk.sh
@@ -2,13 +2,8 @@ echo "checking disk usage on nodes"
 
 NODES=`kubectl get nodes --no-headers | awk '{print $1}'`  
 
-function get_disk_usage {
-  ssh -q -oServerAliveInterval=5 -oConnectTimeout=10 -oConnectionAttempts=1 -oStrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null azureuser@$1 df -$2 | grep sda
-}
-
 for n in $NODES;do
-   echo "node: $n"
-   get_disk_usage $n "h"
-   get_disk_usage $n "i"
+   echo $n
+   ssh -oServerAliveInterval=5 -oConnectTimeout=10 -oConnectionAttempts=1 -oStrictHostKeyChecking=no $n df -h | grep sda
+   ssh -oServerAliveInterval=5 -oConnectTimeout=10 -oConnectionAttempts=1 -oStrictHostKeyChecking=no $n df -i | grep sda
 done
-
