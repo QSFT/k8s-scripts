f76b74098a957f49c0a2bb9f0b35f6d34336c8cf
Anton Timiskov <anton.timiskov@quest.com>
2018-06-14 12:53:23 +0000

    Slack notification based on pod statuses

--- slack.sh
+++ slack.sh
@@ -10,19 +10,28 @@ echo $NAME
 date
 
 KUBE=$(kubectl get node && \
-    sudo etcdctl cluster-health && \
-    ./nodes_disk.sh && \
-    ./pods.sh $NAMESPACES)
+       sudo etcdctl cluster-health && \
+       ./nodes_disk.sh)
+
+PODS=$(./pods.sh $NAMESPACES)
 
 STATUS=":red_circle:"
 NOTIFICATION='<!channel> '
 
-BAD=$(echo $KUBE | grep -E "NotReady|unhealthy")
+BAD=$(echo $PODS | grep -E "NotReady|unhealthy")
 if [ ${#BAD} -eq 0 ];then
     STATUS=":green_apple:"
     NOTIFICATION=''
+fi
+
+KUBE="$KUBE\n$PODS"
+BAD=$(echo $KUBE | grep -E "NotReady|unhealthy")
+if [ ${#BAD} -eq 0 ];then
     KUBE='cluster is healthy'
 fi
 
-RES=$(date && echo ${NOTIFICATION}$STATUS" "*$NAME* && echo "${KUBE}")
-curl -XPOST $SLACK_URL -d "{\"text\":\"$RES\"}";
+
+rm -f /tmp/slackmsg
+RES="{\"text\":\"$(date && echo ${NOTIFICATION}$STATUS" "*$NAME* && echo "${KUBE}")\"}"
+echo $RES > /tmp/slackmsg
+curl -XPOST $SLACK_URL -d "@/tmp/slackmsg";
