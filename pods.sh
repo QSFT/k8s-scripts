851e8c687d2703f19ce7c899a4eb09ea56016286
Anton Timiskov <anton.timiskov@quest.com>
2018-06-14 12:35:55 +0000

    Show unhealthy pod names

--- pods.sh
+++ pods.sh
@@ -2,11 +2,11 @@
 NAMESPACES=$1
 
 function getPods {
-  PODS=`kubectl get pods $* -o json | jq '[.items[] | { name: .metadata.name, node: .spec.nodeName, status: .status.conditions[] | select(.status=="False") } | select( now - (.status.lastTransitionTime | fromdateiso8601) > 300 )] | group_by(.node) | map({ node: .[0].node, total: length })'`
+  PODS=`kubectl get pods $* -o json | jq '[.items[] | { pod: .metadata.name, node: .spec.nodeName, namespace: .metadata.namespace, status: .status.conditions[] | select(.status=="False") } | select( now - (.status.lastTransitionTime | fromdateiso8601) > 300 )] | map({ namespace: .namespace, pod: .pod }) | group_by(.namespace) | map({ namespace: .[0].namespace, pods: [.[].pod] })'`
 
   if [ ${#PODS} -gt 2 ]; then
     echo "unhealthy pods: "
-    echo $PODS | jq -r '.[] | "\(.node)\t\(.total) pods"'
+    echo $PODS | jq -r '.[] | "namespace: \(.namespace): \(.pods) pods"'
   else
     echo "All pods are healthy"
   fi
